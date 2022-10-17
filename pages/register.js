6a07ebb9642db5487dab6915558c4dd78115545f
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2022-10-17 07:11:01 +0600

    login page designed

---
+++ pages/register.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Register = () => {
+  return (
+    <div>
+      {" "}
+      <Head>Login</Head>
+      <h1>Register</h1>
+    </div>
+  );
+};
+
+export default Register;
